04fa6723178e511c0990a7ae03967e1e8d42e5ca
CoderEven <huangjinwen@JinwendeMacBook-Pro.local>
2015-10-21 00:32:45 +0800

    初始化项目
    1、添加项目icon
    2、添加项目启动图片

---
+++ QQMusic/QQMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQMusic
+//
+//  Created by 黄进文 on 15/10/21.
+//  Copyright © 2015年 黄进文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
