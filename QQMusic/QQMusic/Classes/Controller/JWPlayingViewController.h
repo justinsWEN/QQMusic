2a5b3b95ee20c697b05c9b19d5628db15e5f6754
CoderEven <huangjinwen@JinwendeMacBook-Pro.local>
2015-10-21 03:02:18 +0800

    初始化界面
    初始化界面

---
+++ QQMusic/QQMusic/Classes/Controller/JWPlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  JWPlayingViewController.h
+//  QQMusic
+//
+//  Created by 黄进文 on 15/10/21.
+//  Copyright © 2015年 黄进文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JWPlayingViewController : UIViewController
+
+@end
